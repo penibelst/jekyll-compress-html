f2fcce5f09b1d91e9002b79825318119581ff7b4
Anatol Broder <anatol@penibelst.de>
2014-05-30 22:14:10 +0200

    Create plugin

---
+++ compress_html.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require 'rubygems'
+require 'nokogiri'
+
+
+module Jekyll
+  module CompressHTMLFilter
+
+    def compress_html(input)
+      doc = Nokogiri::HTML(input)
+      doc.inner_html
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CompressHTMLFilter)
