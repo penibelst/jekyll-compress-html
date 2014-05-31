ba851292fca2a4675936d4d254dae3bcf7bf9799
Anatol Broder <anatol@penibelst.de>
2014-05-31 03:15:35 +0200

    Tests run

--- test/test_filter.rb
+++ test/test_filter.rb
@@ -1,14 +1,28 @@
 require 'test/unit'
+require 'shoulda'
 
 class TestFilter < Test::Unit::TestCase
-  def assert_file(file)
-    rendered = File.join('_site', file)
-    assertion = File.join('assertion', file)
-    assert File.identical?(rendered, assertion), File.new(rendered)
+  def file_content(file, directory)
+    path = File.join('test', directory, file)
+    File.new(path).read.rstrip
   end
 
-  context "Empty" do
-    should "render empty file to empty" do
+  def expected_content(file)
+    file_content(file, 'expectation')
+  end
+
+  def rendered_content(file)
+    file_content(file, '_site')
+  end
+
+  def assert_file(file)
+    rendered = rendered_content(file)
+    expected = expected_content(file)
+    assert_equal(expected, rendered)
+  end
+
+  context 'Empty' do
+    should 'render empty file to empty' do
       assert_file 'empty.html'
     end
   end
