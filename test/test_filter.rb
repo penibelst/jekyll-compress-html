bc5d07676ed236e018d75f0f4eed80499f1339e7
Anatol Broder <anatol@penibelst.de>
2014-05-31 02:32:56 +0200

    Remove do

--- test/test_filter.rb
+++ test/test_filter.rb
@@ -1,5 +1,7 @@
+require 'test/unit'
+
 class TestFilter < Test::Unit::TestCase
-  def assert_file(file) do
+  def assert_file(file)
     rendered = File.join('_site', file)
     assertion = File.join('assertion', file)
     assert File.identical?(rendered, assertion), File.new(rendered)
