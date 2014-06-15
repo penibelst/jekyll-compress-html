21e8efc7b85bfabef6f9aca5121d195168886872
Anatol Broder <anatol@penibelst.de>
2014-06-15 09:31:02 +0200

    Move jekyll task into tests

--- Rakefile
+++ Rakefile
@@ -1,13 +1,7 @@
 require 'rake'
 require 'rake/testtask'
 
-task :jekyll do
-  Dir.chdir('test') do
-    sh 'jekyll build'
-  end
-end
-
-Rake::TestTask.new(:test => :jekyll) do |t|
+Rake::TestTask.new(:test) do |t|
   t.libs << 'test'
   t.test_files = FileList['test/test_*.rb']
   t.verbose = true
--- test/test_filter.rb
+++ test/test_filter.rb
@@ -12,7 +12,17 @@ class TestCompressed < Test::Unit::TestCase
     assert_equal file_content(file, 'expected'), file_content(file, 'compressed'), file
   end
 
+  def jekyll_build
+    Dir.chdir(File.dirname(__FILE__)) do
+      %x{jekyll build}
+    end
+  end
+
   # Tests
+  def setup
+    jekyll_build
+  end
+
   def test_compressed
     Dir.glob("test/expected/*.html") do |path|
       assert_file File.basename(path)
